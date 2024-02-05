af8882ac985ab108afc175e8d46bd5d7322e8ac8
Vasyl Diachuk <djachukvasja@gmail.com>
2024-02-05 20:26:22 +0200

    1

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,37 @@
+import clsx from "clsx";
+import css from "./Profile.module.css"
+
+export const Profile=({name,tag,location,image,stats}) => {
+    return (
+        
+        <div className={css.profile}>
+            <div className="description">
+                <img src={image}
+                    alt="User avatar"
+                    className="avatar"
+                />
+                <p className="name">{name}</p>
+                <p className="tag">{tag}</p>
+                <p className="location">{location}</p>
+            </div>
+
+
+            <ul className="stats">
+                <li>
+                    <span className="label">Followers</span>
+                    <span className="value">{stats.followers}</span>
+                </li>
+                <li>
+                    <span className="label">Views</span>
+                    <span className="value">{stats.views}</span>
+                </li>
+                <li>
+                    <span className="label">Likes</span>
+                    <span className="value">{stats.likes}</span>
+                </li>
+            </ul>
+        </div>
+        
+    );
+};
+
